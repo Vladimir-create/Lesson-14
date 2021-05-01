215c32019356046166ca6d080e5f576a702d4d7b
Vladimir-create <79356244+Vladimir-create@users.noreply.github.com>
2021-05-01 11:44:47 +0300

    Create websocketclient.go

---
+++ websocketclient.go
@@ -0,0 +1,58 @@
+
+package main
+
+import (
+	"log"
+	//"time"
+	"fmt"
+	"github.com/gorilla/websocket"
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+	//nickname = []byte{'Player1'}
+)
+
+
+func readMess() {
+	c, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/socket", nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+	
+	for {
+		_, p, err := c.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Printf("recv: %s", p)
+	}
+}
+
+
+func writeMess() {
+	c, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/socket2", nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+	var s string
+	
+	for {
+		fmt.Scan(&s)
+		err := c.WriteMessage(websocket.TextMessage, []byte(s) )
+		if err != nil {
+			log.Fatal("dial:", err)
+		}
+	}
+}
+
+
+func main (){
+	go writeMess()
+	go readMess()
+}
+
